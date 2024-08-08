cb3db482f6a40539fefbf588bb7d498a1dba40c3
ROY <101548778+Nightcatclub@users.noreply.github.com>
2024-08-08 11:45:36 +1000

    Week3.1
    Week3.1

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,12 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
-import JSON from './components/JSON.vue'
+// import HelloWorld from './components/HelloWorld.vue'
+// import TheWelcome from './components/TheWelcome.vue'
+// import JSON from './components/JSON.vue'
+import FORM from './components/FORM.vue'
 </script>
 
 <template>
-  <header>
+  <!-- <header>
     <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
 
     <div class="wrapper">
@@ -16,7 +17,8 @@ import JSON from './components/JSON.vue'
 
   <main>
     <TheWelcome />
-  </main>
+  </main> -->
+  <FORM />
 </template>
 
 <style scoped>
