0a93b7089a63088132bca4d3795ea40b128d860f
ROY <101548778+Nightcatclub@users.noreply.github.com>
2024-08-28 13:06:50 +1000

    Week5-Efolio5-P/C

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,22 @@
 <script setup>
-// import HelloWorld from './components/HelloWorld.vue'
-// import TheWelcome from './components/TheWelcome.vue'
-// import JSON from './components/JSON.vue'
-import FORM from './components/FORM.vue'
+import JSONLab from './components/JSONLab.vue'
+import BHeader from './components/BHeader.vue'
+import LibraryRegistrationForm from './components/LibraryRegistrationForm.vue'
 </script>
 
 <template>
-  <!-- <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-      <JSON></JSON>
-    </div>
+  <header>
+    <BHeader />
   </header>
 
   <main>
-    <TheWelcome />
-  </main> -->
-  <FORM />
+    <LibraryRegistrationForm />
+    <!-- <JSONLab /> -->
+  </main>
 </template>
 
 <style scoped>
-header {
+/* header {
   line-height: 1.5;
 }
 
@@ -47,5 +41,5 @@ header {
     place-items: flex-start;
     flex-wrap: wrap;
   }
-}
+} */
 </style>
--- src/main.js
+++ src/main.js
@@ -1,19 +1,19 @@
 // import './assets/main.css'
-// import './style.css'
+// import '@/assets/style.css'
+import 'bootstrap/dist/css/bootstrap.min.css'
 
 import { createApp } from 'vue'
 import App from './App.vue'
 
-import 'bootstrap/dist/css/bootstrap.min.css'
-
 import PrimeVue from 'primevue/config'
 import Aura from '@primevue/themes/aura'
+// import DataTable from 'primevue/datatable'
+// import Column from 'primevue/Column'
 
 const app = createApp(App)
-app.use(PrimeVue, {
-  theme: {
-    preset: Aura
-  }
-})
+app.use(PrimeVue, { theme: { preset: Aura } })
 
-createApp(App).mount('#app')
+// app.component('DataTable', DataTable)
+// app.component('Column', Column)
+
+app.mount('#app')
