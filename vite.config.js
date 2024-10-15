015db33e3569096b607486b8b23f087652ef8ccc
ROY <101548778+Nightcatclub@users.noreply.github.com>
2024-10-16 01:26:11 +1100

    final

--- vite.config.js
+++ vite.config.js
@@ -1,27 +1,23 @@
 // import { fileURLToPath, URL } from 'node:url'
-
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
-
-// eslint-disable-next-line no-undef
-module.exports = {
-  // eslint-disable-next-line no-undef
-  publicPath: process.env.NODE_ENV === 'production'
-    ? '/rchen-library/'
-    : '/'
-}
+import { fileURLToPath, URL } from 'node:url'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [
-    vue(),
-    vueDevTools(),
-  ],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
+export default defineConfig(({ mode }) => {
+  return {
+    plugins: [
+      vue(),
+      vueDevTools(),
+    ],
+    resolve: {
+      alias: {
+        '@': fileURLToPath(new URL('./src', import.meta.url))
+      }
+    },
+    base: mode === 'production'
+      ? '/rchen-library/'
+      : '/'
   }
 })
-
