bcd567be9ab15308b327a5fea94d9fd69f13d825
ROY <101548778+Nightcatclub@users.noreply.github.com>
2024-10-16 01:14:52 +1100

    Update AboutView.vue

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="about">
-    <h1 class="text-center">About Our Library</h1>
+    <h1 class="text-center">About Our Library1</h1>
     <p class="text-center">
       Welcome to our digital library! We're dedicated to providing a vast collection of books and
       resources to our community.
